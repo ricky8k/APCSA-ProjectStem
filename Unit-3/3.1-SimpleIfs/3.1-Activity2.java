088bdab397da9acd9935051713a9bb94aa0d6d70
Ricky <92121005+ricky8k@users.noreply.github.com>
2022-10-19 18:29:58 -0400

    Add files via upload

---
+++ Unit-3/3.1-SimpleIfs/3.1-Activity2.java
@@ -0,0 +1,22 @@
+/* Lesson 1 Coding Activity Question 2 */
+
+import java.util.Scanner;
+
+public class U3_L1_Activity_Two
+{
+  public static void main(String[] args)
+  {
+    // Initialize Scanner
+    Scanner scan = new Scanner(System.in);
+    
+    // User Input
+    System.out.println("Please enter an integer:");
+    int x = scan.nextInt();
+    
+    // Final Output
+    if (x == 48)
+    {
+      System.out.println("YES");
+    }
+  }
+}
---
+++ Unit-3/README.md
@@ -0,0 +1,4 @@
+# Unit 3: Boolean Expressions and If Statements
+
+## Contents
+- 3.1 (Lesson 1) - Simple Ifs
