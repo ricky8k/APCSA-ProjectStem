81571a7548f12f0adb6cf7995555fea6864ba1d2
Ricky <92121005+ricky8k@users.noreply.github.com>
2022-12-06 11:27:53 -0500

    Initial README & Add Unit 5.1/5.2

---
+++ Unit-5/5.1-VoidMethods/5.1-Activity1.java
@@ -0,0 +1,12 @@
+/* Lesson 1 Coding Activity Question 1 */
+
+import java.util.Scanner;
+
+public class U5_L1_Activity_One
+{
+  public static void myMethod()
+  {
+    // Final Output
+    System.out.println("I'm a void method!");
+  }
+}
---
+++ Unit-5/README.md
@@ -0,0 +1,5 @@
+# Unit 5: Iteration
+
+## Contents
+- 5.1 (Lesson 1) - Void Methods
+- 5.2 (Lesson 2) - Parameters
