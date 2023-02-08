525152ca75c14aef687528c227fa1ec3af22d4e0
Ricky <92121005+ricky8k@users.noreply.github.com>
2023-02-07 21:40:56 -0500

    Add Unit 7.1

---
+++ Unit-7/7.1-ArrayList/7.1-Activity1.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class U7_L1_Activity_One
+{
+
+  public static void main(String[] args)
+  {
+    // Initialize Scanner
+    Scanner scan = new Scanner(System.in);
+    
+    // Create Variables
+    ArrayList<String> words = new ArrayList<String>();
+    String input = "";
+    
+    // User Input
+    System.out.println("Please enter words, enter STOP to stop the loop.");
+    while (!(input.equals("STOP")))
+    {
+      input = scan.nextLine();
+      // Check input
+      if (!(input.equals("STOP")))
+        words.add(input);
+    }
+    
+    // Final Output
+    System.out.println("\n" + words.size());
+    System.out.println(words);
+    /// Modify words
+    if (words.size() > 2)
+    {
+      words.set(words.size() - 1, words.get(0));
+      words.remove(0);
+    }
+    System.out.println(words);
+  }
+
+}
