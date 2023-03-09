83dafb8817ae1e68114ca7a0cc93050aac5c9d2c
Ricky <92121005+ricky8k@users.noreply.github.com>
2023-03-08 19:19:40 -0500

    refactor: Cleanup Activity 1 for Unit 6.1

--- Unit-6/6.1-OneDimensionalArrays/Activity1/U6_L1_Activity_One.java
+++ Unit-6/6.1-OneDimensionalArrays/Activity1/U6_L1_Activity_One.java
@@ -8,14 +8,14 @@ public class U6_L1_Activity_One
     Scanner scan = new Scanner(System.in);
     
     // Create Array
-    double[] arr = new double[4];
+    double[] arr = new double[3];
     /// User Input
+    arr[0] = scan.nextDouble();
     arr[1] = scan.nextDouble();
     arr[2] = scan.nextDouble();
-    arr[3] = scan.nextDouble();
     
     // Final Output
-    System.out.println("Contents: " + arr[1] + " " + arr[2] + " " + arr[3]);
-    System.out.println("Sum: " + (arr[1] + arr[2] + arr[3]));
+    System.out.println("Contents: " + arr[0] + " " + arr[1] + " " + arr[2]);
+    System.out.println("Sum: " + (arr[0] + arr[1] + arr[2]));
   }
 }
