ab6a4d14371afda9fcf576a03283a9f8a112adc5
Ricky <92121005+ricky8k@users.noreply.github.com>
2023-03-17 15:53:06 -0400

    feat: Add Runners for Unit 10

---
+++ Unit-10/10.1-IntroToRecursion/Activity1/runner_U10_L1_Activity_One.java
@@ -0,0 +1,17 @@
+// Provided code from Project STEM.
+// No modifications were made to this file.
+
+import java.util.Scanner;
+public class runner_U10_L1_Activity_One
+{
+
+  public static void main(String[] args)
+  {
+    System.out.println("Enter positive integer:");
+    Scanner scan = new Scanner(System.in);
+    int i = scan.nextInt();
+    U10_L1_Activity_One.printUpToEnd(i);
+    System.out.println();
+  }
+  
+}
---
+++ Unit-10/10.2-RecursiveFunctionsWithReturns/Activity1/runner_U10_L2_Activity_One.java
@@ -0,0 +1,16 @@
+// Provided code from Project STEM.
+// No modifications were made to this file.
+
+import java.util.Scanner;
+public class runner_U10_L2_Activity_One
+{
+
+  public static void main(String[] args)
+  {
+    System.out.println("Enter string:");
+    Scanner scan = new Scanner(System.in);
+    String s = scan.nextLine();
+    System.out.println("Reversed String: " + U10_L2_Activity_One.stringReverse(s));
+  }
+  
+}
