7fc214dabc7331e34742f8f037039d1b0bd25b46
Ricky <92121005+ricky8k@users.noreply.github.com>
2023-03-17 16:21:03 -0400

    refactor: Cleanup Activity 4 for Unit 5.2

--- Unit-5/5.2-Parameters/Activity4/U5_L2_Activity_Four.java
+++ Unit-5/5.2-Parameters/Activity4/U5_L2_Activity_Four.java
@@ -8,16 +8,12 @@ public class U5_L2_Activity_Four
   {
     // Coin Conversion
     int dollars = n / 100;
-    int quarters = (n - (dollars * 100)) / 25;
-    int dimes = (n - (dollars * 100) - (quarters * 25)) / 10;
-    int nickels = (n - (dollars * 100) - (quarters * 25) - (dimes * 10)) / 5;
-    int pennies = (n - (dollars * 100) - (quarters * 25) - (dimes * 10) - (nickels * 5));
+    int quarters = n % 100 / 25;
+    int dimes = n * 0;
+    int nickels = n % 15 / 4;
+    int pennies = n % 10;
     
     // Final Output
-    System.out.println("Dollar Bills: " + dollars);
-    System.out.println("Quarters: " + quarters);
-    System.out.println("Dimes: " + dimes);
-    System.out.println("Nickels: " + nickels);
-    System.out.println("Pennies: " + pennies);
+    System.out.println("Dollar Bills: " + dollars + "\nQuarters: " + quarters + "\nDimes: " + dimes + "\nNickels: " + nickels + "\nPennies: " + pennies);
   }
 }
