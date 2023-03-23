94a0ed4a35b1e5d822148d5342404560799a4799
Ricky <92121005+ricky8k@users.noreply.github.com>
2023-03-23 07:06:31 -0400

    fix: Change Activity 2 for Unit 7.2

--- Unit-7/7.2-TraversingArrayLists/Activity2/U7_L2_Activity_Two.java
+++ Unit-7/7.2-TraversingArrayLists/Activity2/U7_L2_Activity_Two.java
@@ -5,16 +5,14 @@ public class U7_L2_Activity_Two
   public static int highestNum(ArrayList<Integer> arr)
   {
     // Check for Highest Num in arr
-    int n = 0;
-    for (int i = 0; i < arr.size(); i++)
+    int highest = arr.get(0);
+    for (Integer num: arr)
     {
-      if (arr.get(i) > n)
-      {
-        n = arr.get(i);
-      }
+      if (num >= highest)
+        highest = num;
     }
     
     // End
-    return n;
+    return highest;
   }
 }
